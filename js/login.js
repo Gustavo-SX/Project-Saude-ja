dee1b1bf73064f7323bd051f1349bf8c82a0b113
Samuelhollanda <samueloliveirad337@gmail.com>
2025-09-23 21:42:34 -0300

    tela de login com usuario adcionada

--- js/login.js
+++ js/login.js
@@ -9,3 +9,18 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove('active')
 });
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // Evita recarregar a página
+
+  const user = document.getElementById("user").value;
+  const pass = document.getElementById("password").value;
+  const erro = document.getElementById("fail");
+
+  // Exemplo básico
+  if (user === "admin" && pass === "123") {
+    window.location.href = "index.html";
+  } else {
+    erro.style.display = "block";
+  }
+});
